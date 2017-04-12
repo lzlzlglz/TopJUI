04f7f72f4d0796219eafaf644d7e793ce1420153
小策一喋 <xvpindex@qq.com>
2017-04-12 16:33:40 +0800

    优化代码

--- themes/image/icons/1.bat
+++
@@ -1,5 +0,0 @@
-::═════代═══码═══开═══始═════
-@echo off&setlocal enabledelayedexpansion
-for /r %%i in (*.png) do (
-echo .%%i {background:url^("%%i"^) no-repeat center center; } >> 1.css)
-::═════代═══码═══结═══束═════
