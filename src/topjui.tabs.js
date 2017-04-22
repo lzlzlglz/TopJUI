8883e7c67809969bb96155cfbb20b1b0b3cea970
小策一喋 <xvpindex@qq.com>
2017-04-22 21:34:38 +0800

    优化代码

--- src/topjui.tabs.js
+++ src/topjui.tabs.js
@@ -1,92 +1,94 @@
 (function ($) {
 
     $.fn.iTabs = function (options) {
+
+        var initShow = true;
+
         var defaults = {
             title: '',
             closable: true,
             iconCls: '',
             content: '',
-            //href: '',
+            //href: '/system/page/selectRow',
             border: false,
-            fit: true
-        }
-
-        var options = $.extend(defaults, options);
-        var $tabsElement = $('#' + options.id);
-        var initShow = true;
-
-        $(this).tabs({
-            title: options.title,
-            closable: options.closable,
-            iconCls: options.iconCls,
-            content: options.content,
-            //href: options.href,
-            border: options.border,
-            fit: options.fit,
+            fit: true,
             onSelect: function (title, index) {
-                //初始化显示tabs时，不加载里面的内容
-                if (!initShow) {
-                    var tabs = options.tabs;
-                    var newQueryParams = {};
-                    var $element = $('#' + tabs[index].id);
-                    if (tabs[index].type == "datagrid") {
-                        var gridOptions = $element.datagrid('options');
-                        var $parentGrid = $('#' + gridOptions.parentGrid.id);
-                        if (gridOptions.parentGrid.type == "datagrid")
-                            var selectedRow = $parentGrid.datagrid("getSelected");
-                        if (gridOptions.parentGrid.type == "treegrid")
-                            var selectedRow = $parentGrid.treegrid("getSelected");
-                        if (selectedRow) {
-                            newQueryParams = getSelectedRowJson(gridOptions.parentGrid.param, selectedRow);
-                            //获得表格原有的参数
-                            var queryParams = $element.datagrid('options').queryParams;
-                            $element.datagrid('options').queryParams = $.extend({}, queryParams, newQueryParams);
-                            $element.datagrid('load');
-                        } else {
-                            $element.datagrid('load');
-                        }
-                    } else if (tabs[index].type == "treegrid") {
-                        var gridOptions = $element.panel('options');
-                        var $parentGrid = $('#' + gridOptions.parentGrid.id);
-                        if (gridOptions.parentGrid.type == "datagrid")
-                            var selectedRow = $parentGrid.datagrid("getSelected");
-                        if (gridOptions.parentGrid.type == "treegrid")
-                            var selectedRow = $parentGrid.treegrid("getSelected");
-                        if (selectedRow) {
-                            newQueryParams = getSelectedRowJson(gridOptions.parentGrid.param, selectedRow);
-                            //获得表格原有的参数
-                            var queryParams = $element.datagrid('options').queryParams;
-                            $element.treegrid('options').queryParams = $.extend({}, queryParams, newQueryParams);
-                            $element.treegrid('load');
-                        } else {
-                            $element.treegrid('load');
-                        }
-                    } else if (tabs[index].type == "panel") {
-                        var panelOptions = $element.panel('options');
-                        var $parentGrid = $('#' + panelOptions.parentGrid.id);
-                        if (panelOptions.parentGrid.type == "datagrid")
-                            var selectedRow = $parentGrid.datagrid("getSelected");
-                        if (panelOptions.parentGrid.type == "treegrid")
-                            var selectedRow = $parentGrid.treegrid("getSelected");
-                        if (selectedRow) {
-                            var newHref = replaceUrlParamValueByBrace(panelOptions.dynamicHref, selectedRow);
-                            //$element.panel('refresh', newHref);
-                            var iframe = '<iframe src="' + newHref + '" scrolling="auto" frameborder="0" style="width:100%;height:100%;"></iframe>';
-                            $element.panel({
-                                content: iframe
-                            });
-                        } else {
-                            $element.panel('refresh');
+                var tabs = options.tabs;
+                var $element = $('#' + tabs[index].id);
+
+                var panelOptions = $element.panel('options');
+                if (panelOptions.href != undefined) {
+                    var iframe = '<iframe src="' + panelOptions.href + '" scrolling="auto" frameborder="0" style="width:100%;height:100%;"></iframe>';
+                    $element.panel({
+                        content: iframe
+                    });
+                    $element.panel('refresh');
+                } else {
+                    //初始化显示tabs时，不加载里面的内容
+                    if (!initShow) {
+                        var newQueryParams = {};
+                        if (tabs[index].type == "datagrid") {
+                            var gridOptions = $element.datagrid('options');
+                            var $parentGrid = $('#' + gridOptions.parentGrid.id);
+                            if (gridOptions.parentGrid.type == "datagrid")
+                                var selectedRow = $parentGrid.datagrid("getSelected");
+                            if (gridOptions.parentGrid.type == "treegrid")
+                                var selectedRow = $parentGrid.treegrid("getSelected");
+                            if (selectedRow) {
+                                newQueryParams = getSelectedRowJson(gridOptions.parentGrid.param, selectedRow);
+                                //获得表格原有的参数
+                                var queryParams = $element.datagrid('options').queryParams;
+                                $element.datagrid('options').queryParams = $.extend({}, queryParams, newQueryParams);
+                                $element.datagrid('load');
+                            } else {
+                                $element.datagrid('load');
+                            }
+                        } else if (tabs[index].type == "treegrid") {
+                            var gridOptions = $element.panel('options');
+                            var $parentGrid = $('#' + gridOptions.parentGrid.id);
+                            if (gridOptions.parentGrid.type == "datagrid")
+                                var selectedRow = $parentGrid.datagrid("getSelected");
+                            if (gridOptions.parentGrid.type == "treegrid")
+                                var selectedRow = $parentGrid.treegrid("getSelected");
+                            if (selectedRow) {
+                                newQueryParams = getSelectedRowJson(gridOptions.parentGrid.param, selectedRow);
+                                //获得表格原有的参数
+                                var queryParams = $element.datagrid('options').queryParams;
+                                $element.treegrid('options').queryParams = $.extend({}, queryParams, newQueryParams);
+                                $element.treegrid('load');
+                            } else {
+                                $element.treegrid('load');
+                            }
+                        } else if (tabs[index].type == "panel") {
+                            var panelOptions = $element.panel('options');
+                            var $parentGrid = $('#' + panelOptions.parentGrid.id);
+                            if (panelOptions.parentGrid.type == "datagrid")
+                                var selectedRow = $parentGrid.datagrid("getSelected");
+                            if (panelOptions.parentGrid.type == "treegrid")
+                                var selectedRow = $parentGrid.treegrid("getSelected");
+                            if (selectedRow) {
+                                var newHref = replaceUrlParamValueByBrace(panelOptions.dynamicHref, selectedRow);
+                                //$element.panel('refresh', newHref);
+                                var iframe = '<iframe src="' + newHref + '" scrolling="auto" frameborder="0" style="width:100%;height:100%;"></iframe>';
+                                $element.panel({
+                                    content: iframe
+                                });
+                            } else {
+                                $element.panel('refresh');
+                            }
                         }
                     }
+                    initShow = false;
                 }
-                initShow = false;
-
             },
             onLoad: function (panel) {
                 //$(this).trigger(topJUI.eventType.initUI.base);
             }
-        });
+        }
+
+        var options = $.extend(defaults, options);
+
+        $(this).tabs(options);
     }
 
     // 扩展tabs方法
