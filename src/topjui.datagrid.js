0e99fd9e3f746cf9dfed861b518482e865b5a70a
小策一喋 <xvpindex@qq.com>
2017-04-25 17:17:17 +0800

    优化代码

--- src/topjui.datagrid.js
+++ src/topjui.datagrid.js
@@ -124,6 +124,10 @@
 
             },
             onLoadSuccess: function () {
+
+                //$('#' + options.id).datagrid('doCellTip', {cls: {'background-color': 'red'}, delay: 500});
+                $('#' + options.id).datagrid('doCellTip', {cls: {}, delay: 500});
+
                 //$(this).datagrid("fixRownumber");
                 if (typeof options.childGrid == "object") {
                     var refreshGridIdArr = options.childGrid.grids;
@@ -356,6 +360,114 @@
                     }
                 }
             );
+        },
+        /**
+         * http://blog.csdn.net/aa1049372051/article/details/22849891
+         * 开启消息提示功能
+         * @param {} jq
+         * @param {} params 提示消息框的样式
+         * @return {}
+         */
+        doCellTip: function (jq, params) {
+            function showTip(data, td, e) {
+                if ($(td).text() == "")
+                    return;
+                data.tooltip.text($(td).text()).css({
+                    top: (e.pageY + 10) + 'px',
+                    left: (e.pageX + 20) + 'px',
+                    'z-index': $.fn.window.defaults.zIndex,
+                    display: 'block'
+                });
+            };
+            return jq.each(function () {
+                var grid = $(this);
+                var options = $(this).data('datagrid'); //获取 datagrid 数据
+
+                if (!options.tooltip) {
+                    var panel = grid.datagrid('getPanel').panel('panel');
+                    var defaultCls = {
+                        'border': '1px solid #333',
+                        'padding': '1px',
+                        'color': '#333',
+                        'background': '#f7f5d1',
+                        'position': 'absolute',
+                        'max-width': '400px',
+                        'border-radius': '4px',
+                        '-moz-border-radius': '4px',
+                        '-webkit-border-radius': '4px',
+                        'display': 'none'
+                    }
+                    var tooltip = $("<div id='celltip'></div>").appendTo('body');
+                    tooltip.css($.extend({}, defaultCls, params.cls));
+                    options.tooltip = tooltip;
+                    panel.find('.datagrid-body').each(function () {
+                        var delegateEle = $(this).find('> div.datagrid-body-inner').length
+                            ? $(this).find('> div.datagrid-body-inner')[0]
+                            : this;
+                        $(delegateEle).undelegate('td', 'mouseover').undelegate(
+                            'td', 'mouseout').undelegate('td', 'mousemove')
+                            .delegate('td', {
+                                'mouseover': function (e) {
+                                    if (params.delay) {
+                                        if (options.tipDelayTime)
+                                            clearTimeout(options.tipDelayTime);
+                                        var that = this;
+                                        options.tipDelayTime = setTimeout(
+                                            function () {
+                                                showTip(options, that, e);
+                                            }, params.delay);
+                                    } else {
+                                        showTip(options, this, e);
+                                    }
+
+                                },
+                                'mouseout': function (e) {
+                                    if (options.tipDelayTime)
+                                        clearTimeout(options.tipDelayTime);
+                                    options.tooltip.css({
+                                        'display': 'none'
+                                    });
+                                },
+                                'mousemove': function (e) {
+                                    var that = this;
+                                    if (options.tipDelayTime) {
+                                        clearTimeout(options.tipDelayTime);
+                                        options.tipDelayTime = setTimeout(
+                                            function () {
+                                                showTip(options, that, e);
+                                            }, params.delay);
+                                    } else {
+                                        showTip(options, that, e);
+                                    }
+                                }
+                            });
+                    });
+
+                }
+
+            });
+        },
+        /**
+         * 关闭消息提示功能
+         * @param {} jq
+         * @return {}
+         */
+        cancelCellTip: function (jq) {
+            return jq.each(function () {
+                var data = $(this).data('datagrid');
+                if (data.tooltip) {
+                    data.tooltip.remove();
+                    data.tooltip = null;
+                    var panel = $(this).datagrid('getPanel').panel('panel');
+                    panel.find('.datagrid-body').undelegate('td',
+                        'mouseover').undelegate('td', 'mouseout')
+                        .undelegate('td', 'mousemove')
+                }
+                if (data.tipDelayTime) {
+                    clearTimeout(data.tipDelayTime);
+                    data.tipDelayTime = null;
+                }
+            });
         }
     });
 
