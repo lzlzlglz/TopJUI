b5592c65c16ee38a89fb5d68051b230b8f2c8784
小策一喋 <xvpindex@qq.com>
2017-08-14 16:25:42 +0800

    增加排序功能

--- demo/topjui/js/locale/topjui.lang.zh_CN.js
+++ demo/topjui/js/locale/topjui.lang.zh_CN.js
@@ -3,54 +3,34 @@ if ($.fn.pagination) {
     $.fn.pagination.defaults.afterPageText = '共{pages}页';
     $.fn.pagination.defaults.displayMsg = '显示{from}到{to},共{total}记录';
 }
-
 if ($.fn.datagrid) {
-    $.fn.datagrid.defaults.loadMsg = '正在处理，请稍待。。。';
+    $.fn.datagrid.defaults.loadMsg = '正在加载，请稍待。。。';
 }
-
 if ($.fn.treegrid && $.fn.datagrid) {
     $.fn.treegrid.defaults.loadMsg = $.fn.datagrid.defaults.loadMsg;
 }
-
-if ($.fn.datagrid && $.fn.datagrid.defaults && $.fn.datagrid.defaults.operators.nofilter) {
-    $.fn.datagrid.defaults.operators.nofilter.text = "无";
-    $.fn.datagrid.defaults.operators.contains.text = "包含";
-    $.fn.datagrid.defaults.operators.equal.text = "=等于";
-    $.fn.datagrid.defaults.operators.notequal.text = "!=不等于";
-    $.fn.datagrid.defaults.operators.beginwith.text = "^=以*开始";
-    $.fn.datagrid.defaults.operators.endwith.text = "$=以*结束";
-    $.fn.datagrid.defaults.operators.less.text = "<小于";
-    $.fn.datagrid.defaults.operators.lessorequal.text = "<=小于等于";
-    $.fn.datagrid.defaults.operators.greater.text = ">大于";
-    $.fn.datagrid.defaults.operators.greaterorequal.text = ">=大于等于";
-}
-
 if ($.messager) {
     $.messager.defaults.ok = '确定';
     $.messager.defaults.cancel = '取消';
 }
-
 $.map(['validatebox', 'textbox', 'passwordbox', 'filebox', 'searchbox',
     'combo', 'combobox', 'combogrid', 'combotree',
     'datebox', 'datetimebox', 'numberbox',
     'spinner', 'numberspinner', 'timespinner', 'datetimespinner'], function (plugin) {
     if ($.fn[plugin]) {
-        $.fn[plugin].defaults.missingMessage = '该输入项为必输项';
+        $.fn[plugin].defaults.missingMessage = '必填';
     }
 });
-
 if ($.fn.validatebox) {
     $.fn.validatebox.defaults.rules.email.message = '请输入有效的电子邮件地址';
     $.fn.validatebox.defaults.rules.url.message = '请输入有效的URL地址';
     $.fn.validatebox.defaults.rules.length.message = '输入内容长度必须介于{0}和{1}之间';
     $.fn.validatebox.defaults.rules.remote.message = '请修正该字段';
 }
-
 if ($.fn.calendar) {
     $.fn.calendar.defaults.weeks = ['日', '一', '二', '三', '四', '五', '六'];
     $.fn.calendar.defaults.months = ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月'];
 }
-
 if ($.fn.datebox) {
     $.fn.datebox.defaults.currentText = '今天';
     $.fn.datebox.defaults.closeText = '关闭';
@@ -74,7 +54,6 @@ if ($.fn.datebox) {
         }
     };
 }
-
 if ($.fn.datetimebox && $.fn.datebox) {
     $.extend($.fn.datetimebox.defaults, {
         currentText: $.fn.datebox.defaults.currentText,
@@ -82,7 +61,19 @@ if ($.fn.datetimebox && $.fn.datebox) {
         okText: $.fn.datebox.defaults.okText
     });
 }
-
 if ($.fn.datetimespinner) {
     $.fn.datetimespinner.defaults.selections = [[0, 4], [5, 7], [8, 10], [11, 13], [14, 16], [17, 19]]
+}
+
+if ($.fn.datagrid && $.fn.datagrid.defaults && $.fn.datagrid.defaults.operators.nofilter) {
+    $.fn.datagrid.defaults.operators.nofilter.text = "无";
+    $.fn.datagrid.defaults.operators.contains.text = "包含";
+    $.fn.datagrid.defaults.operators.equal.text = "=等于";
+    $.fn.datagrid.defaults.operators.notequal.text = "!=不等于";
+    $.fn.datagrid.defaults.operators.beginwith.text = "^=以*开始";
+    $.fn.datagrid.defaults.operators.endwith.text = "$=以*结束";
+    $.fn.datagrid.defaults.operators.less.text = "<小于";
+    $.fn.datagrid.defaults.operators.lessorequal.text = "<=小于等于";
+    $.fn.datagrid.defaults.operators.greater.text = ">大于";
+    $.fn.datagrid.defaults.operators.greaterorequal.text = ">=大于等于";
 }
